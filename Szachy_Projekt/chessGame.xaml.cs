54f014846cae21a76486d4d17e1ba1b0643d3c58
Your Name <you@example.com>
2023-12-25 22:11:46 +0100

    Knight works

--- Szachy_Projekt/chessGame.xaml.cs
+++ Szachy_Projekt/chessGame.xaml.cs
@@ -39,67 +39,51 @@ namespace Szachy_Projekt
     /// Logika interakcji dla klasy chessGame.xaml
     /// </summary>
 
-    struct ChessboardParameters
+        public struct param
     {
-        private Button[,] buttons;
-        private figureValue[,] position;
-        //private figureValue[] blackPiecesCannotCapture;
-        //private figureValue[] whitePiecesCannotCapture;
-        public Button button { get; set; }
-        public Button FirstClick { get; set; }
-        public Button SecondClick { get; set; }
-        public int CurrentRow { get; set; }
-        public int CurrentColumn { get; set; }
 
-        public Button[,] Buttons
+        public static Button[,] Buttons { get; set; }
+        public static figureValue[,] Position { get; set; }
+        public static figureValue[] BlackPiecesAttacked { get; set; }
+        public static figureValue[] WhitePiecesAttacked { get; set; }
+        public static figureValue[] BlackPieces { get; set; }
+        public static figureValue[] WhitePieces { get; set; }
+        public static Button Button { get; set; }
+        public static Button FirstClick { get; set; }
+        public static Button SecondClick { get; set; }
+        public static int CurrentRow { get; set; }
+        public static int CurrentColumn { get; set; }
+        public static bool GlobalTurn {  get; set; }
+
+        static param()
         {
-            get { return buttons; }
-            set { buttons = value; }
-        }
+        
+            }
 
-        public figureValue[,] Position
+        public static void Initialize()
         {
-            get { return position; }
-            set { position = value; }
-        }
 
-        //public figureValue[] BlackPiecesCannotCapture
-        //{
-        //    get { return blackPiecesCannotCapture; }
-        //    set { blackPiecesCannotCapture = value; }
-        //}
+            GlobalTurn = true;
+            BlackPiecesAttacked = [figureValue.BlackPawn, figureValue.BlackKnight, figureValue.BlackBishop, figureValue.BlackRook, figureValue.BlackQueen];
+            WhitePiecesAttacked = [figureValue.WhitePawn, figureValue.WhiteKnight, figureValue.WhiteBishop, figureValue.WhiteRook, figureValue.WhiteQueen];
 
-        //public figureValue[] WhitePiecesCannotCapture
-        //{
-        //    get { return whitePiecesCannotCapture; }
-        //    set { blackPiecesCannotCapture = value; }
-        //}
-
-        public ChessboardParameters(Button[,] buttons, figureValue[,] position, Button firstClick, Button secondClick, int currentRow, int currentColumn, figureValue[] blackPiecesCannotCapture, figureValue[] whitePiecesCannotCapture)
-        {
-            this.buttons = buttons;
-            this.position = position;
-            FirstClick = firstClick;
-            SecondClick = secondClick;
-            CurrentRow = currentRow;
-            CurrentColumn = currentColumn;
-
-            //BlackPiecesCannotCapture = blackPiecesCannotCapture ?? new figureValue[] { figureValue.BlackPawn, figureValue.BlackKnight, figureValue.BlackBishop, figureValue.BlackKnight, figureValue.BlackQueen, figureValue.WhiteKing };
-            //WhitePiecesCannotCapture = whitePiecesCannotCapture ?? new figureValue[] { figureValue.WhitePawn, figureValue.WhiteKnight, figureValue.WhiteBishop, figureValue.WhiteKnight, figureValue.WhiteQueen, figureValue.BlackKnight };
+            BlackPieces = [figureValue.BlackPawn, figureValue.BlackKnight, figureValue.BlackBishop, figureValue.BlackRook, figureValue.BlackQueen, figureValue.BlackKing];
+            WhitePieces = [figureValue.WhitePawn, figureValue.WhiteKnight, figureValue.WhiteBishop, figureValue.WhiteRook, figureValue.WhiteQueen, figureValue.WhiteKing];
         }
     }
+
 
 
     public partial class chessGame : Page
     {
 
-        private ChessboardParameters param = new ChessboardParameters();
         public chessGame()
         {
             InitializeComponent();
- 
-            generateChessboard();
             
+            param.Initialize();
+
+            generateChessboard();
 
         }
 
@@ -111,161 +95,34 @@ namespace Szachy_Projekt
             int column = Grid.GetColumn(button);
             int row = Grid.GetRow(button);
 
-            BlackKnightMove(row, column, button);
-
-        }
-
-        private void BlackKnightMove(int row, int column, Button button)
-        {
-            figureValue[] blackPiecesAttacked = new figureValue[] { figureValue.BlackPawn,figureValue.BlackRook, figureValue.BlackKnight, figureValue.BlackBishop, figureValue.BlackQueen};
-            figureValue[] whitePiecesAttacked = new figureValue[] { figureValue.WhitePawn,figureValue.WhiteRook, figureValue.WhiteKnight, figureValue.WhiteBishop, figureValue.WhiteQueen };
+            //figureClass figure = new figureClass();
 
 
 
-
-            if ( param.FirstClick == null && param.Position[row, column] == figureValue.BlackKnight)
+            //figure.BlackKnightMove(row, column, button);
+           
+           
+                    
+                
+            if(param.GlobalTurn == true)
             {
-
-                for (int i = 0; i < 8; i++)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-
-                        if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 == 0)
-                        {
-                            param.Buttons[i, j].Background = Brushes.Gray;
-                        }
-                        else if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 != 0)
-                        {
-                            param.Buttons[i, j].Background = Brushes.White;
-                        }
-
-                        if (param.Buttons[i, j].Content is System.Windows.Controls.Image image && image.Source == Images.Dot)
-                        {
-                            param.Buttons[i, j].Content = "";
-                        }
-
-
-                    }
-                }
-
-                param.FirstClick = button;
-                param.CurrentRow = row;
-                param.CurrentColumn = column;
-
-               
-                // BlackKnightCheck = true;
-                int futureRow = 0;
-                int futureColumn = 0;
-
-                int[] knightMoves = { -2, -1, 1, 2 };
-
-                foreach (int i in knightMoves)
-                {
-                    foreach (int j in knightMoves)
-                    {
-                        
-                        if (Math.Abs(i) + Math.Abs(j) != 3)
-                        {
-                            continue;
-                        }
-
-                            futureRow = row + i;
-                            futureColumn = column + j;
-
-                      
-                        if ((futureRow >= 0 && futureRow < 8 && futureColumn >= 0 && futureColumn < 8) && param.Position[futureRow,futureColumn] == figureValue.Empty)
-                        {
-                            param.Buttons[futureRow, futureColumn].Content = new System.Windows.Controls.Image { Source = Images.Dot };
-                            
-                        }
-                        else if (((futureRow >= 0 && futureRow < 8 && futureColumn >= 0 && futureColumn < 8)) && whitePiecesAttacked.Contains(param.Position[futureRow,futureColumn]))
-                        {
-                           param.Buttons[futureRow, futureColumn].Background = Brushes.Red;
-                        }
-                    }
-                }
-
+                Knight whiteKnight = new Knight();
+                whiteKnight.FigureMove(row, column, button, figureValue.WhiteKnight, Images.WhiteKnight, param.BlackPiecesAttacked);
             }
-            else if ((param.FirstClick != null) && (param.SecondClick == null) && ((param.Buttons[row, column].Content is System.Windows.Controls.Image image && image.Source == Images.Dot || (whitePiecesAttacked.Contains(param.Position[row, column])))))
+            else if(param.GlobalTurn == false)
             {
-
-                param.SecondClick = button;
-
-                param.Buttons[param.CurrentRow, param.CurrentColumn].Content = "";
-                param.Buttons[row, column].Content = new System.Windows.Controls.Image { Source = Images.BlackKnight }; ;
-                param.Position[row, column] = figureValue.BlackKnight;
-                param.Position[param.CurrentRow, param.CurrentColumn] = 0;
-
-                param.FirstClick = null;
-                param.SecondClick = null;
-                // BlackKnightCheck = false;
-                // GlobalTurn = true;
-
-
-                for (int i = 0; i < 8; i++)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-
-                        if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 == 0)
-                        {
-                            param.Buttons[i, j].Background = Brushes.Gray;
-                        }
-                        else if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 != 0)
-                        {
-                            param.Buttons[i, j].Background = Brushes.White;
-                        }
-
-                        if (param.Buttons[i, j].Content is System.Windows.Controls.Image image1 && image1.Source == Images.Dot)
-                        {
-                            param.Buttons[i, j].Content = "";
-                        }
-
-
-                    }
-                }
-
-            }
-            else
-            {
-                param.FirstClick = null;
-                param.SecondClick = null;
-                // BlackKnightCheck = false;
-
-
-                for (int i = 0; i < 8; i++)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-
-                        if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 == 0)
-                        {
-                            param.Buttons[i, j].Background = Brushes.Gray;
-                        }
-                        else if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 != 0)
-                        {
-                            param.Buttons[i, j].Background = Brushes.White;
-                        }
-
-                        if (param.Buttons[i, j].Content is System.Windows.Controls.Image image2 && image2.Source == Images.Dot)
-                        {
-                            param.Buttons[i, j].Content = "";
-                        }
-
-
-                    }
-                }
-
+                Knight blackKnight = new Knight();
+                blackKnight.FigureMove(row, column, button, figureValue.BlackKnight, Images.BlackKnight, param.WhitePiecesAttacked);
             }
 
         }
 
+      
 
 
         private void generateChessboard()
         {
-            //ChessboardParameters chessboardParameters = new ChessboardParameters();
+            
 
             Button[,] buttons = new Button[8,8];
             figureValue[,] position = new figureValue[8, 8];
@@ -275,7 +132,7 @@ namespace Szachy_Projekt
                 for (int j = 0; j < 8; j++)
                 {
                     Button button = new Button();
-                    param.button = button;
+                    param.Button = button;
 
                     Grid.SetRow(button, i);
                     Grid.SetColumn(button, j);
---
+++ Szachy_Projekt/figureClass.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Szachy_Projekt
+{
+    public abstract class figureClass
+    {
+        protected void HelpMoveClear()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+
+                    if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 == 0)
+                    {
+                        param.Buttons[i, j].Background = Brushes.Gray;
+                    }
+                    else if (param.Buttons[i, j].Background == Brushes.Red && (i + j) % 2 != 0)
+                    {
+                        param.Buttons[i, j].Background = Brushes.White;
+                    }
+
+                    if (param.Buttons[i, j].Content is System.Windows.Controls.Image image && image.Source == Images.Dot)
+                    {
+                        param.Buttons[i, j].Content = "";
+                    }
+
+
+                }
+            }
+        }
+
+        protected abstract void LegalMoves(int row, int column, figureValue[] figureAttacked);
+
+
+        protected void ShowLegalMoves(int futureRow, int futureColumn, figureValue[] figureAttacked)
+        {
+            if ((futureRow >= 0 && futureRow < 8 && futureColumn >= 0 && futureColumn < 8) && param.Position[futureRow, futureColumn] == figureValue.Empty)
+            {
+                param.Buttons[futureRow, futureColumn].Content = new System.Windows.Controls.Image { Source = Images.Dot };
+
+            }
+            else if (((futureRow >= 0 && futureRow < 8 && futureColumn >= 0 && futureColumn < 8)) && figureAttacked.Contains(param.Position[futureRow, futureColumn]))
+            {
+                param.Buttons[futureRow, futureColumn].Background = Brushes.Red;
+            }
+        }
+       
+        public void FigureMove(int row, int column, Button button, figureValue figure, ImageSource figureImage ,figureValue[] figureAttacked)
+        {
+
+            {
+
+                if (param.FirstClick == null && param.Position[row, column] == figure)
+                {
+
+                    HelpMoveClear();
+
+                    param.FirstClick = button;
+                    param.CurrentRow = row;
+                    param.CurrentColumn = column;
+                    
+
+                    LegalMoves(row, column, figureAttacked);
+
+                }
+
+                
+                else if ((param.FirstClick != null) && (param.SecondClick == null) && ((param.Buttons[row, column].Content is System.Windows.Controls.Image image && image.Source == Images.Dot || param.Buttons[row, column].Background == Brushes.Red )))
+                {
+
+                    param.SecondClick = button;
+
+                    param.Buttons[param.CurrentRow, param.CurrentColumn].Content = "";
+                    param.Buttons[row, column].Content = new System.Windows.Controls.Image { Source = figureImage }; ;
+                    param.Position[row, column] = figure;
+                    param.Position[param.CurrentRow, param.CurrentColumn] = 0;
+
+                    param.FirstClick = null;
+                    param.SecondClick = null;
+                    
+                    HelpMoveClear();
+
+                    if(param.BlackPieces.Contains(figure))
+                    {
+                        param.GlobalTurn = true;
+                    }
+                    else if(param.WhitePieces.Contains(figure))
+                    {
+                        param.GlobalTurn = false;
+                    }
+
+                }
+                else
+                {
+                    param.FirstClick = null;
+                    param.SecondClick = null;
+
+                    HelpMoveClear();
+
+                }
+
+            }
+
+
+        }
+
+
+
+    }
+}
